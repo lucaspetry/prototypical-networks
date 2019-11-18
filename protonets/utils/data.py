24c4794e94a7a3bd74cc0e20f30b3ebaba40e0d0
Lucas May Petry <lucasmaypetry@gmail.com>
2019-11-18 16:57:38 -0400

    Added Cifar100 and MiniImageNet datasets

--- protonets/utils/data.py
+++ protonets/utils/data.py
@@ -3,6 +3,10 @@ import protonets.data
 def load(opt, splits):
     if opt['data.dataset'] == 'omniglot':
         ds = protonets.data.omniglot.load(opt, splits)
+    elif opt['data.dataset'] == 'miniimagenet':
+        ds = protonets.data.miniimagenet.load(opt, splits)
+    elif opt['data.dataset'] == 'cifar100_few':
+        ds = protonets.data.cifar100.load(opt, splits)
     else:
         raise ValueError("Unknown dataset: {:s}".format(opt['data.dataset']))
 
